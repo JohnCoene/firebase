dcafa862288e42c18ddc8dc95e682ed4d4db26a9
munoztd0 <david.munoztord@mailbox.org>
2023-06-30 18:33:28 +0200

    rebuild web site

--- site/docify.R
+++
@@ -1,6 +0,0 @@
-news <- readLines("NEWS.md")
-news <- gsub("#", "##", news)
-writeLines(news, "docs/misc/changelog.md")
-
-fs::file_copy("LICENSE.md", "docs/misc/license.md", overwrite = TRUE)
-fs::file_copy("CODE_OF_CONDUCT.md", "docs/misc/coc.md", overwrite = TRUE)
